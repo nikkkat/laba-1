8b634e2b8e6d4e6ad3a56a0fbba3dc150067967c
nikkkat <96502274+nikkkat@users.noreply.github.com>
2022-03-10 01:52:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+int a = first / second;
+Console.WriteLine("Результат деления: " + a);
